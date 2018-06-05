548f19e1833202e5c0264b98271dae597128e140
Laura Daufeld <laura.daufeld@gmail.com>
2018-06-05 16:14:27 +0200

    add map

--- js/front.js
+++ js/front.js
@@ -10,7 +10,7 @@ $(function () {
     utils();
     sliding();
     contactForm();
-    // map();
+    map();
     counters();
     parallax();
     demo();
@@ -351,9 +351,10 @@ function map() {
     var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
 	el: '#map',
-	lat: -12.043333,
-	lng: -77.028333,
-	zoomControl: true,
+	lat: 47.322047,
+	lng: 5.041480,
+	zoom: 7,
+	zoomControl: false,
 	zoomControlOpt: {
 	    style: 'SMALL',
 	    position: 'TOP_LEFT'
@@ -370,8 +371,8 @@ function map() {
     var image = 'img/marker.png';
 
     map.addMarker({
-	lat: -12.043333,
-	lng: -77.028333,
+	lat: 47.322047,
+	lng: 5.041480,
 	icon: image/* ,
 	 title: '',
 	 infoWindow: {
